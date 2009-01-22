d7e8f9b576f2adcd48a8409726ad3fdf50e9ec9b
Steven Wilkin <stevenwilkin@gmail.com>
2009-01-22 18:54:51 +0000

    google analytics

--- www/index.php
+++ www/index.php
@@ -61,5 +61,15 @@ $modified = date('D d/m/Y G:i', filemtime(DATA));
 
 <img src="/img/<?php echo $image ?>" title="<?php echo $description ?>" alt="<?php echo $description ?>" />
 
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-2764682-6");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+
 </body>
 </html>
