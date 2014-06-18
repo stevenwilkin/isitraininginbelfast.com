ed99a7628d8ef037cc5cf9acbcac2dcd56f42e17
Steven Wilkin <stevenwilkin@gmail.com>
2014-06-18 21:25:33 +0100

    Use a POST to /update to update the forecast

--- app.rb
+++ app.rb
@@ -1,14 +1,35 @@
 require 'sinatra/base'
 require 'redis'
+require 'nokogiri'
+require 'open-uri'
 
 class App < Sinatra::Base
 	set :logging, true
 
+  helpers do
+    def redis
+      @redis ||= begin
+        uri = URI.parse(ENV["REDISTOGO_URL"] || '')
+        Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+      end
+    end
+
+    def update
+      # list of possible conditions: http://developer.yahoo.com/weather/#codes
+      doc = Nokogiri::XML(open('http://weather.yahooapis.com/forecastrss?w=44544'))
+      # current weather condition
+      condition = doc.xpath('//yweather:condition').attr('text').value
+      condition =~ /rain|shower|storm/i
+      redis.set('raining', !$~.nil?)
+    end
+  end
+
   get '/' do
-    uri = URI.parse(ENV["REDISTOGO_URL"] || '')
-    redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     @is_raining = redis.get('raining') == 'true'
     erb :index
   end
 
+  post '/update' do
+    update
+  end
 end
